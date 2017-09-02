cf8d69026605491c15661f6a9d9f2b33f4c3cf03
TrilangDev <teste@trilang.com>
2017-09-02 19:08:27 -0300

    INSERIR E ALTERAR

--- class/Usuario.php
+++ class/Usuario.php
@@ -39,6 +39,20 @@ class Usuario {
         $this->dtcadastro = $value;
     }
 
+    public static function getList() {
+        $sql = new Sql();
+        
+        return $sql->select('SELECT * FROM tb_usuarios ORDER BY deslogin');
+
+    }
+
+    public static function search($login) {
+        $sql = new Sql();
+        return $sql->select('SELECT * FROM tb_usuarios WHERE deslogin LIKE :SEARCH ORDER BY deslogin', array(
+            ':SEARCH'=>'%'.$login.'%'
+        ));
+    }
+
     public function loadById($id) {
 
         $sql = new Sql();
@@ -49,14 +63,71 @@ class Usuario {
 
         if (count($results) > 0) {
 
-            $row = $results[0];
-
-            $this->setIdUsuario($row['idusuario']);
-            $this->setDesLogin($row['deslogin']);
-            $this->setDesSenha($row['dessenha']);
-            $this->setDtCadastro(new DateTime($row['dtcadastro']));
+            $this->setData($results[0]);
 
         }
+    }
+
+    public function login($login, $password) {
+        $sql = new Sql();
+        
+            $results = $sql->select('SELECT * FROM tb_usuarios WHERE deslogin = :LOGIN AND dessenha = :PASSWORD', array(
+                ':LOGIN'=>$login,
+                ':PASSWORD'=>$password
+            ));
+    
+            if (count($results) > 0) {
+    
+                $this->setData($results[0]);
+        
+            } else {
+                throw new Exception('Login e/ou senha inválidos.');
+            }
+    }
+
+    public function setData($data) {
+
+        $this->setIdUsuario($data['idusuario']);
+        $this->setDesLogin($data['deslogin']);
+        $this->setDesSenha($data['dessenha']);
+        $this->setDtCadastro(new DateTime($data['dtcadastro']));
+
+    }
+
+    public function insert() {
+
+        $sql = new Sql();
+
+        $results = $sql->select('CALL sp_usuarios_insert(:LOGIN, :PASSWORD)', array(
+            ':LOGIN'=>$this->getDesLogin(),
+            ':PASSWORD'=>$this->getDesSenha()
+        ));
+
+        if (count($results) > 0) {
+            $this->setData($results[0]);
+        }
+
+
+    }
+
+    public function update($login, $password) {
+
+        $this->setDesLogin($login);
+        $this->setDesSenha($password);
+
+        $sql = new Sql();
+
+        $sql->query('UPDATE tb_usuarios SET deslogin = :LOGIN, dessenha = :PASSWORD WHERE idusuario = :ID', array(
+            ':LOGIN'=>$this->getDesLogin(),
+            ':PASSWORD'=>$this->getDesSenha(),
+            ':ID'=>$this->getIdUsuario()
+        ));
+
+    }
+
+    public function __construct($login = '', $password = '') {
+        $this->setDeslogin($login);
+        $this->setDesSenha($password);
     }
 
     public function __toString() {
--- index.php
+++ index.php
@@ -2,10 +2,46 @@
 
 require_once('config.php');
 
-$root = new Usuario();
+
+//CARREGA UM USUÁRIO
+/*$root = new Usuario();
 
 $root->loadbyId(3);
 
-echo $root;
+echo $root;*/
+
+//CARREGA UMA LISTA DE USUÁRIOS
+/*$lista = Usuario::getList();
+
+echo json_encode($lista);*/
+
+//CARREGA LISTA USUARIO BUSCANDO PELO LOGIN
+/*$search = Usuario::search('Us');
+
+echo json_encode($search);*/
+
+//CARREGA UM USUÁRIO USANDO LOGIN/SENHA
+/*$usuario = new Usuario();
+
+$usuario->login('geral', '12348765');
+
+echo $usuario;*/
+
+//INSERIR USUARIO NO BD
+/*
+$aluno = new Usuario('aluno','124');
+
+$aluno->insert();
+
+echo $aluno;
+*/
+
+$usuario = new Usuario();
+
+$usuario->loadById(8);
+
+$usuario->update('novo', '12348765');
+
+echo $usuario;
 
 ?>
